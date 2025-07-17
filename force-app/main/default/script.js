9a0ea597164dff11e9c323649d7d9a9e90fd387b
ajitha-99 <ajithag9999@gmail.com>
2025-07-17 15:47:08 -0500

    COMMIT

--- force-app/main/default/script.js
+++ force-app/main/default/script.js
@@ -1,8 +1,9 @@
-function beforesubmit(){
-   let outputdate =  document.queryselector(".outputdate");
+function beforesubmit() {
+   let outputdate =  document.querySelector(".outputdate");
    let inputdate = document.querySelector(".inputdate");
    console.log("inputdate.value", inputdate.value);//string -->date(en_US)
 
-   let formattedDate = new Date(inputdate.value).toLocaleDateString('en-US');
+   let formattedDate = new Date(inputdate.value).toLocaleDateString("en-US");
    outputdate.value = formattedDate;
+   console.log(outputdate.value);
 }
